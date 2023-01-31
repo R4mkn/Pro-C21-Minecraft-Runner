5bbf62bdf66a23c7410d99513593f8263832464e
R4mkn <119461706+R4mkn@users.noreply.github.com>
2023-01-31 00:09:58 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//variaveis chatasassasasasa
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
@@ -34,34 +35,40 @@ function preload(){
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
-
+   // Background
    Background = createSprite(width-545, height-250)
    Background.addAnimation("Background", backgroundImg)
    Background.scale = 6
-   Background.velocityX = (4 + 20*score/50);   
+   //SteveRunning
    Steve = createSprite(width-540, height-190);
    Steve.addAnimation("steverunning", steveImg);
+   //InvisibleGround
    Iground = createSprite(width-100,height-120)
    Iground.addImage("Ground", igroundImg)
    Iground.scale = 1.5
+   //SteveJumping
    Steve.debug = false
    Steve.addAnimation("Jumping", stevejumpImg)
+   //Creeper
    Creeper = createSprite(width-80, height-190)
    Creeper.addAnimation("CreeperRunning", creeperImg)
-  
+   //Died
    Died = createSprite(width-530,height-220)
    Died.addImage("DiedScreen", diedImg)  
    Died.scale = 3 
    Died.visible = false
+   //Restart
    Restart = createSprite(width-530,height-200)
    Restart.addImage("Restart", restartImg)
    Restart.visible = false
+   //SteveDied
    Steve.addImage("SteveDied", stevediedImg);
+   //You Lose
    Lose = createSprite(width-520,height-340)
    Lose.addImage("YouLose", loseImg)
    Lose.scale = 0.5
    Lose.visible = false
-
+   //RockGroup
    rockGroup = new Group();
 
 
@@ -70,100 +77,96 @@ function setup(){
 
 function draw() {
   background("blue");
-
+// Gamestate Play
 if (gameState===PLAY){
+  //SISTEMA DE SCORE
   score = score + Math.round(getFrameRate()/60);
-
+  //MOVENDO O BACKGROUND
   Background.velocityX = (6 + 3*score/150);
 
-
+  //BACKGROUND SE DIVIDINDO
   if(Background.x > 1400 ){
     Background.x = height/2;
   }  
+  //SISTEMA DE PULO
   if((touches.length > 0 || keyDown("SPACE")) && Steve.y  >= height-240) {
     Steve.velocityY = -10;
     Steve.changeAnimation("Jumping", stevejumpImg)
     touches = [];
     
   }
+
+   //GRAVIDADE DO STEVE 
     Steve.velocityY = Steve.velocityY + 0.8
    
-   
+   //SPAWNAR ROCHAS
     spawnRock()
 
+   //STEVE MORRENDO 
     if(Steve.isTouching(rockGroup)){
+      hitSound.play()
       gameState = END
 
     }
+   //CREEPER PULANDO AS ROCHAS 
     if(Creeper.isTouching(rockGroup)){
-      Creeper.velocityY = -15;
+      Creeper.velocityY = -20;
     }
      Creeper.velocityY = Creeper.velocityY + 0.8
 
   
 }
 
-
-  
+//ESTADO DO JOGO NO FINAL  
 else if (gameState === END) {
-   
+  //VELOCIDADES PARADAS 
   rockGroup.velocityX = 0     
   Background.velocityX = 0
   steveImg.velocityY = 0
-  Restart.visible = true
+
+  //TROCAR A ANIMAÇAO PARA MORTEEEEEEEE
   Steve.changeAnimation("SteveDied", stevediedImg)
+
+  //VISIBILIDADE 
+  Restart.visible = true
   Died.visible = true
   Lose.visible = true
   rockGroup.setLifetimeEach(0)
-
+  
+  // BOTAO RESTART
   if(mousePressedOver(Restart)) {      
     reset();
   }
-
-
-
 }
-
-
-
-Creeper.velocityY = Creeper.velocityY + 0.8
-
 
 
   drawSprites();
 
+
+  //TEXTO DO SCORE
   textSize(23)
   fill(255);
   textFont('Minecraft');
   text("score: "+score,width-130,height-20);
 
 
-    
+    //COLLISAO DOS PERSONAGENSSSSSSSSSSSSSSSSSSSSSSS
     Steve.setCollider("rectangle", 0, 20, 200, 0, 0);
     Creeper.setCollider("rectangle", 0, 20, 200, 0, 0);
     Steve.collide(Iground);
     Creeper.collide(Iground);
 
 
-    Creeper.debug = false
-    
-
-
-
-
-
-
-
-
-    
+    //SOLO INVISIVEL
     Iground.visible = false
 
 }
-
+//QUANDO O BOTAO FOR SOLTADO
 function keyReleased(){
   Steve.changeAnimation("steverunning", steveImg)
 }
 
+//SISTEMA DE ROCHASSS
 function spawnRock(){
   if (frameCount % 60 === 0) {
     var Rock = createSprite(width-1640,height-90)  
@@ -182,6 +185,8 @@ function spawnRock(){
   }
 }
 
+
+//RENICIAR O JOGO
 function reset(){
   gameState = PLAY;
   Lose.visible = false;
